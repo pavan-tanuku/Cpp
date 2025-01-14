3917b1ca2f70dd064956361078d1a94e4ec00f39
pavan-tanuku <pavan.tanuku@sasi.ac.in>
2025-01-14 17:10:22 +0530

    Added user input/output statements in C++

---
+++ IO.cpp
@@ -0,0 +1,16 @@
+/*
+Input and Output program in C++
+Author: Pavan Mani Sankar
+Date: 2025-01-14
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter the two values : ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "The sum of " << a << " and " << b << " is : " << c << "\n";
+    return 0;
+}
