77e68693ff588b9093385e5fbcae7e47365bfa54
pavan-tanuku <pavan.tanuku@sasi.ac.in>
2025-01-14 17:00:27 +0530

    Added HelloWorld Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*
+Hello World Program in C++
+Author: Pavan Mani Sankar
+Date: 2021-01-14
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s = "Hello, World!";
+    cout << s << "\n";
+    cout << s.length() << "\n";
+    cout << s.size() << "\n";
+    return 0;
+}
